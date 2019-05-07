0f48baf3bbb387dd540bd00705dc5efe2c30daba
WinstonMarvel <winston@winstonmarvel.com>
2019-05-07 12:54:02 +0530

    Fixed issue #12, #13 - Achieved status in report shows correct colors - Exported sheet does not contain the additional worksheet

--- server/routes/export/sheetWriter.js
+++ server/routes/export/sheetWriter.js
@@ -1,5 +1,20 @@
 var Excel = require('exceljs');
 
+let colors = {
+    success: {
+        text: 'ff006100',
+        bg: 'ffc6efce'
+    },
+    neutral: {
+        text: 'ff9c5700',
+        bg: 'ffffeb9c'
+    },
+    failure: {
+        text: 'ff9c0006',
+        bg: 'ffffc7ce'
+    }
+};
+
 function writeToExcel( obj, sheetSchema, formType, worksheet ){
     obj.postInfo.blogType = formType.toUpperCase();
     if(formType == "ebp"){
@@ -26,12 +41,40 @@ function writeToExcel( obj, sheetSchema, formType, worksheet ){
             cell.value = obj[prop];
         }
     }
+    let statusCell = worksheet.getCell('B8');
+    let colorScheme;
+    switch( statusCell.value.toLowerCase() ){
+        case "achieved":
+        colorScheme = "success";
+        break;
+        case "did not achieve":
+        colorScheme = "failure";
+        break;
+        case "partially achieved":
+        colorScheme = "neutral";
+        break;
+    }
+    statusCell.font = {
+        color : {  argb: colors[colorScheme].text }
+    };
+    statusCell.fill = {
+        type: 'pattern',
+        pattern:'solid',
+        fgColor: { argb: colors[colorScheme].bg },
+        bgColor: { argb: colors[colorScheme].bg }
+    };
 };
 
 module.exports = function( post, sheetSchema, formType, callback ){
     var workbook = new Excel.Workbook();
-    workbook.xlsx.readFile('./routes/export/reviewTemplate.xlsx')
-    .then( () => {
+    var templatePath;
+    if( formType.toUpperCase() === "EBP" ) {
+        templatePath = './routes/export/reviewTemplate-EBP.xlsx';
+    }
+    else{
+        templatePath = './routes/export/reviewTemplate-TBP.xlsx';
+    }
+    workbook.xlsx.readFile(templatePath).then( () => {
         var worksheet = workbook.getWorksheet(formType.toUpperCase());
         console.log(worksheet);
         writeToExcel( post, sheetSchema, formType, worksheet);
